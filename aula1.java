1c6aadbdcdd42790c94a35cac599a98722d49a9a
Heinrich37 <belforthenrich2004@gmail.com>
2025-07-23 19:28:09 -0400

    .

--- aula1.java
+++ aula1.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 // PRINTAR INTEIROS %d
 // PRINTAR STRING %S
 // PRINTAR DOUBLE %f
-public class Main {
+public class aula1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite seu nome: ");
+        System.out.print("Digite seu nome: ");
         String nome = sc.nextLine();
         System.out.println("Olá, " + nome);
         sc.close();
